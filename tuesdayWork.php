e4ef5c16db028654e598b36afc330ad0aab51fd8
DEMENYACA <39350941+DEMENYACA@users.noreply.github.com>
2019-02-19 07:37:26 -0500

    Add files via upload

---
+++ tuesdayWork.php
@@ -0,0 +1,71 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+// echo "Twinkle, Twinkle little star."
+
+// $twinkle= "twinkle";
+// $star= "star";
+// echo "twinkle, twinkle little $star <br/>";
+
+// $twinkle= "Demi";
+// $star="Morton";
+// echo "$twinkle, $twinkle, you are a  $star !!";
+
+// $x= 10;
+// $y= 7;
+// $result=$x+$y;
+// echo "$x+$y = $result <br/>";
+
+//  $x= 10;
+//  $y= 7;
+//  $result = $x-$y;
+//  echo "$x-$y = $result <br/>";
+ 
+//  $x=10;
+//  $y=7;
+//  $result = $x*$y;
+//  echo "$x*$y = $result <br/> ";
+
+//  $value = 8;
+//  $val = 2;
+//  $result= $value+ $val;
+// echo "$value + $val = $result <br/>";
+
+
+// $value= 10;
+// $valz = 4;
+// $result = $value - $valz;
+// echo "$value - $valz = $result <br/>";
+
+// $value=6;
+// $valz=5;
+// $result = $value * $valz;
+// echo "$value * $valz = $result  <br/>";
+
+
+// Hint: In the script each statement ends with "Value is now $variable."
+
+// Value is now 8.
+// Add 2. Value is now 10.
+// Subtract 4. Value is now 6.
+// Multiply by 5. Value is now 30.
+// Divide by 3. Value is now 10.
+// Increment value by one. Value is now 11.
+// Decrement value by one. Value is now 10.
+
+$value = 8;
+echo "Value is now $value <br/>";
+
+$value =8;
+$valz= 2
+
+    ?>
+</body>
+</html>
